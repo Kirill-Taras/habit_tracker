4be393cc4f1d497fc9ff9ea3534500b2ed3cce4b
kiril_tarasov <kirilltaras1792@yandex.ru>
2025-08-13 11:29:59 +0700

    Проверка flake8 в ci.yml

--- config/settings.py
+++ config/settings.py
@@ -13,13 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
